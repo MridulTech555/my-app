3f251d001bb62028d649fbd607309c94490f120a
mridul <mridulkumar555@gmail.com>
2020-09-22 23:16:25 +0530

    Updates

---
+++ precache-manifest.72307339e58557f2350fbc3bcc4203e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72661c99f27c1d9b976c92e877986c3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "58f24b7812fa9fddadc6",
+    "url": "/my-app/static/css/main.0de117e3.chunk.css"
+  },
+  {
+    "revision": "a5984849e331d1b5a50a",
+    "url": "/my-app/static/js/2.bf3c3d3f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-app/static/js/2.bf3c3d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f24b7812fa9fddadc6",
+    "url": "/my-app/static/js/main.831b1b6a.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
